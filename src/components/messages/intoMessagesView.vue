d12c6ac4afb08a2e1cd999de4b2a1c69266803e0
Diego sepulveda <dasvv7@gmail.com>
2023-06-16 20:21:02 -0500

    changes front aplication

--- src/components/messages/intoMessagesView.vue
+++ src/components/messages/intoMessagesView.vue
@@ -4,9 +4,11 @@ import axios from "../../api/axios";
 import avatarUser from "../shared/avatarUser.vue";
 import { useRoute } from "vue-router";
 import Swal from "sweetalert2";
-
+import { useCounterStore } from "../../store/users";
+const message = ref("");
 const route = useRoute();
 const messages = ref([]);
+
 onMounted(async () => {
   const user = await axios
     .get("/messages?id=" + route.params.id)
@@ -19,29 +21,43 @@ onMounted(async () => {
         timer: 2000,
       });
     });
-
   const response = user.data.data;
+  console.log("mensajes", response);
   messages.value = response;
-
-  console.log(route.params);
 });
+
+const newMessage = async () => {
+  const user = await axios
+    .post("/messages", {
+      message: message.value,
+      conversationId: route.params.id,
+    })
+    .catch((error) => {
+      Swal.fire({
+        icon: "error",
+        title: "Ocurrio un error",
+        text: "Error al enviar el mensaje",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    });
+  messages.value.push(user.data.data);
+};
 </script>
 <template>
   <div class="intoMessages">
+    <div class="intoMessages__messages">
+      <div
+        class="intoMessages__messageItem"
+        v-for="(message, index) in messages"
+        :key="index"
+      >
+        {{ message.message }}
+      </div>
+    </div>
     <div class="intoMessages__container">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, quis
-      voluptatum! Dolor reiciendis labore eveniet ex doloremque quam excepturi
-      quasi, necessitatibus quis aliquam rerum nisi. Atque dolorum sequi unde
-      dolore? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt,
-      quis voluptatum! Dolor reiciendis labore eveniet ex doloremque quam
-      excepturi quasi, necessitatibus quis aliquam rerum nisi. Atque dolorum
-      sequi unde dolore? Lorem ipsum, dolor sit amet consectetur adipisicing
-      elit. Sunt, quis voluptatum! Dolor reiciendis labore eveniet ex doloremque
-      quam excepturi quasi, necessitatibus quis aliquam rerum nisi. Atque
-      dolorum sequi unde dolore? Lorem ipsum, dolor sit amet consectetur
-      adipisicing elit. Sunt, quis voluptatum! Dolor reiciendis labore eveniet
-      ex doloremque quam excepturi quasi, necessitatibus quis aliquam rerum
-      nisi. Atque dolorum sequi unde dolore?
+      <input type="text" placeholder="Buscar" v-model="message" />
+      <button @click="newMessage">Enviar</button>
     </div>
   </div>
 </template>
